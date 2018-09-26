4b4a93a7f9faa0a888572ef7d9b16a011f27b755
longle <longle@teqnological.asia>
2018-09-26 10:09:50 +0700

    update gradle and fix resources

--- app/src/main/java/vn/eazy/bottomnavigation/example/TestFragment.kt
+++ app/src/main/java/vn/eazy/bottomnavigation/example/TestFragment.kt
@@ -16,7 +16,7 @@ class TestFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return LayoutInflater.from(context).inflate(R.layout.fragment_main, null)
     }
 }
